b3b1d82f7f9edae3e85c3f8e50c86db6664d1ec2
Ulan <aukdevgm@gmail.com>
2025-02-18 00:13:14 +0600

    add env api url

--- src/services/seminarService.ts
+++ src/services/seminarService.ts
@@ -1,21 +1,24 @@
 import { Seminar } from '../types/seminar';
 
+const API_URL =
+    import.meta.env.VITE_API_URL || 'http://localhost:5000/seminars';
+
 export const seminarService = {
     async fetchSeminars() {
-        const response = await fetch('api/seminars');
+        const response = await fetch(API_URL);
         if (!response.ok) throw new Error('Не удалось получить семинары.');
         return response.json();
     },
 
     async deleteSeminar(id: number) {
-        const response = await fetch(`api/seminars/${id}`, {
+        const response = await fetch(`${API_URL}/${id}`, {
             method: 'DELETE',
         });
         if (!response.ok) throw new Error('Не удалось удалить семинар.');
     },
 
     async updateSeminar(updatedSeminar: Seminar) {
-        const response = await fetch(`api/seminars/${updatedSeminar.id}`, {
+        const response = await fetch(`${API_URL}/${updatedSeminar.id}`, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(updatedSeminar),
